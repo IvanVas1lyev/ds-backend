6b12690814ca081a9f79f45f828026e0fa5a251b
IvanVas1lyev <vanya71161@gmail.com>
2024-04-04 09:12:32 +0000

    homework backend

---
+++ src/consts.py
@@ -0,0 +1,11 @@
+import logging
+
+LOGGING_FORMAT = '[%(levelname)s] [%(asctime)s] %(message)s'
+LOGGING_LEVEL = logging.INFO
+
+APP_HOST = '0.0.0.0'
+APP_PORT = 8080
+
+IMAGE_HOST = '178.154.220.122'
+IMAGE_PORT = 7777
+IMAGE_SERVER_TIMEOUT = 0.5
